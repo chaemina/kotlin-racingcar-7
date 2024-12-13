c6f10acc676c1571910b861dcc1841a870d58820
chaemina <chaemina82@naver.com>
2024-12-13 20:47:26 +0900

    fix

--- src/main/kotlin/racingcar/controller/MainController.kt
+++ src/main/kotlin/racingcar/controller/MainController.kt
@@ -5,17 +5,17 @@ import racingcar.view.InputView
 import racingcar.view.OutputView
 
 fun MainController() {
-		val inputView = InputView()
-		val outputView = OutputView()
+  val inputView = InputView()
+  val outputView = OutputView()
 
-		outputView.prompt(Constants.PROMPT1)
-		val carList = inputView.getUserInput()
-		val carController = CarController(carList)
-		val cars = carController.createCar()
+  outputView.prompt(Constants.PROMPT1)
+  val carList = inputView.getUserInput()
+  val carController = CarController(carList)
+  val cars = carController.createCar()
 
-		outputView.prompt(Constants.PROMPT2)
-		val attempts = inputView.getAttempts()
+  outputView.prompt(Constants.PROMPT2)
+  val attempts = inputView.getAttempts()
 
-		val racingGame = RacingGame(cars, attempts, outputView)
-		racingGame.start()
+  val racingGame = RacingGame(cars, attempts, outputView)
+  racingGame.start()
 }
